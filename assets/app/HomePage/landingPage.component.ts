3aa6bf7b9aa7c2285910999e3dadb50080134ac8
Nil Shah <nil_shah1992@outlook.com>
2017-02-23 15:01:43 -0500

    Added autocomplete location field in landing page

--- assets/app/HomePage/landingPage.component.ts
+++ assets/app/HomePage/landingPage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Injectable } from "@angular/core";
 import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
@@ -10,12 +10,13 @@ import { LandingPage } from '../profile/landingpage.model';
     templateUrl: 'landingPage.component.html'
 })
 
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+    lplocation:string;
 
     constructor(private _route: ActivatedRoute, private _router: Router){}
+    
 
     onFind(form: NgForm): void{
-        
         const lp = new LandingPage(form.value.lpspeciality, form.value.lplocation);
         let navigationExtras: NavigationExtras = {
             queryParams:{
@@ -25,6 +26,30 @@ export class LandingPageComponent {
         };
 
         this._router.navigate(['/physicianlocator'], navigationExtras);
+    }
+
+    ngOnInit() {
+
+        // Initialize the search box and autocomplete
+        let searchBox: any = document.getElementById('lplocation');
+        let options = {
+            types: [
+                // return only geocoding results, rather than business results.
+                'geocode',
+            ]
+        };
+        var autocomplete = new google.maps.places.Autocomplete(searchBox, options);
+
+       // Add listener to the place changed event
+        autocomplete.addListener('place_changed', () => {
+            let place = autocomplete.getPlace();
+            let lat = place.geometry.location.lat();
+            let lng = place.geometry.location.lng();
+            let address = place.formatted_address;
+            var fields = address.split(',');
+            this.lplocation=fields[0];
+            // this.placeChanged(lat, lng, address);
+        });
     }
 
 }
